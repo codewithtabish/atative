461e9fe6cd58ab3b6172bf0b93223c28339cbf73
codewithtabish <kashisultan099@gmail.com>
2026-08-20 10:29:16 +0500

    feat(refactor): chnaged the conatienr ui again

--- src/components/(app)/(common)/layout/container.tsx
+++ src/components/(app)/(common)/layout/container.tsx
@@ -7,10 +7,7 @@ interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
 
 export function Container({ children, className, ...props }: ContainerProps) {
   return (
-    <div
-      className={cn("mx-auto w-full max-w-360", "px-[clamp(1rem,4vw,3rem)]", className)}
-      {...props}
-    >
+    <div className={cn("mx-auto w-full max-w-360", "px-4 sm:px-5 md:px-6  ", className)} {...props}>
       {children}
     </div>
   );
